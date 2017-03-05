15901238eff387fac11937ba09f92a0ada3fbe66
Julian Vogels <julian@soundbrenner.comgit>
2017-03-05 02:24:44 +0100

    Ups

--- src/components/buzz/ShareButtons.js
+++ src/components/buzz/ShareButtons.js
@@ -1,22 +1,52 @@
-import React, {PropTypes} from 'react'
+import React, {PropTypes, Component} from 'react'
 import {StyleSheet, Text, View, TouchableHighlight} from 'react-native'
 import {SocialIcon} from 'react-native-elements'
+import {ShareDialog} from 'react-native-fbsdk';
 
-export default function ShareButtons ({style, postContentURL}) {
-  return (
-    <View style={[styles.container, style]}>
-      {/*<TouchableHighlight onPress={() => { onPressFacebook(postContentURL); } }>*/}
-        <SocialIcon style={styles.socialIcon} type='facebook' title='Teilen' button/>
-      {/*</TouchableHighlight>*/}
-      <SocialIcon style={styles.socialIcon} type='twitter' title="Tweet" button/>
 
-    </View>
-  )
+export default class ShareButtons extends Component {
+  render () {
+    const {
+      postContentURL,
+      style
+    } = this.props;
+
+    return (
+      <View style={[styles.container, style]}>
+          <SocialIcon style={styles.socialIcon} type='facebook' title='Teilen' button onPress={() => { onPressFacebook(postContentURL); } }/>
+        <SocialIcon style={styles.socialIcon} type='twitter' title="Tweet" button/>
+
+      </View>
+    )
+  }
 }
 
 function onPressFacebook(postContentURL) {
+    var tmp = this;
 
-  // share with facebook
+    var shareLinkContent = {
+      contentType: 'link',
+      contentUrl: postContentURL,
+      contentDescription: 'MEGA'
+    }
+    ShareDialog.canShow(shareLinkContent).then(
+      function(canShow) {
+        if (canShow) {
+          return ShareDialog.show(shareLinkContent);
+        }
+      }
+    ).then(
+      function(result) {
+        if (result.isCancelled) {
+          alert('Share cancelled');
+        } else {
+          alert('Share success with postId: ' + result.postId);
+        }
+      },
+      function(error) {
+        alert('Share fail with error: ' + error);
+      }
+    );
 }
 
 const styles = StyleSheet.create({
